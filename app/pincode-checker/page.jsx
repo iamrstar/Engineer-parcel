5f290ec809847165d87925eb56c773aec10994d7
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2025-09-25 00:28:32 +0530

    Update page.jsx
    aaded some more pincodes

--- app/pincode-checker/page.jsx
+++ app/pincode-checker/page.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import { CheckCircle, Search, XCircle } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card" 
 import { Input } from "@/components/ui/input"  
 import { Label } from "@/components/ui/label"
 
