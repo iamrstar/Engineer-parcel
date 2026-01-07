50ddcf61a759b26f7021bce786ff6bfb53678143
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2026-01-07 22:51:54 +0530

    Add files via upload

---
+++ src/data/cities.js
@@ -0,0 +1,7 @@
+export const cities = [
+  { slug: "dhanbad", city: "Dhanbad", state: "Jharkhand", areas: "Saraidhela, Bank More, Hirapur, Katras, Jharia" },
+  { slug: "ranchi", city: "Ranchi", state: "Jharkhand", areas: "Lalpur, Doranda, Kanke, Harmu" },
+  { slug: "bokaro", city: "Bokaro", state: "Jharkhand", areas: "Sector 4, Sector 9, Chas" },
+];
+
+export default cities;
